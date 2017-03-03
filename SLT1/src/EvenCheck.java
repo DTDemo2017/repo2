56808426701a1c9d0832f121e967990c266acbaf
= <=>
2017-03-03 21:17:11 +0530

    Programs run in SLT class

---
+++ SLT1/src/EvenCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenCheck {
+
+	public static void main(String[] args) {
+		
+		
+			  int var1;
+			Scanner scan= new Scanner(System.in);
+			System.out.println("Enter a number");
+			var1= scan.nextInt();
+			if (var1%2== 0)
+			System.out.println("EvenNumber");
+			else
+			System.out.println("Odd Number");
+			}
+
+	}
+
+
---
+++ SLT2/src/IfLadder.java
@@ -0,0 +1,23 @@
+
+public class IfLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch;
+		ch= 'A';
+		if(ch== 'A')
+		                 System.out.println("Vowel");
+		else if(ch== 'E')
+		                 System.out.println("Vowel");
+		else if(ch== 'I')
+		                 System.out.println("Vowel");
+		else if(ch== 'O')
+		                 System.out.println("Vowel");
+		else if(ch== 'U')
+		                 System.out.println("Vowel");
+		else
+		                 System.out.println("Consonant");
+
+	}
+
+}
