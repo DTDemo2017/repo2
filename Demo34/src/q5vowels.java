0ee7e330c91eb5c71a72e00b1ecde9971daa7e6f
= <=>
2017-03-16 22:25:41 +0530

    Some java programs

---
+++ Demo34/src/q5vowels.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class q5vowels {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String str;
+		int vowels = 0, digits = 0, blanks = 0;
+		char ch;
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("Enter a String : ");
+		str= br.readLine();
+
+		for(int i = 0; i < str.length(); i ++)
+		{
+			ch = str.charAt(i);
+			if(ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || 
+			ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+				vowels ++;
+			
+		 if(Character.isDigit(ch))
+				digits ++;
+			else if(Character.isWhitespace(ch))
+				blanks ++;
+		}
+
+		System.out.println("Vowels : " + vowels);
+		System.out.println("Digits : " + digits);
+		System.out.println("Blanks : " + blanks);
+
+	}
+
+}
